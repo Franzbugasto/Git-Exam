22262df59e18939433404ad5e44e09c3345c883e
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-30 16:44:00 +0800

    Create user profile.js

---
+++ user profile.js
@@ -0,0 +1,9 @@
+let fullName = "Franz Gann Marc V.Bugasto";
+let age = 21;
+let favoriteNumber = 7;
+let favoriteColor = "Red";
+
+console.log("Full Name:",fullName);
+console.log("Age:",age);
+console.log("Favorite Number:",favoriteNumber);
+console.log("Favorite Color:",favoriteColor);
